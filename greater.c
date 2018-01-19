11f3ebf8994baa35cf44b28aacaea8604dc9274a
snivetha81 <31503968+snivetha81@users.noreply.github.com>
2018-01-19 16:05:15 +0530

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+main()
+
+{
+
+int a,b,c;
+
+scanf("%d,%d,%d",&a,&b,&c);
+
+if(a>b&&a>c)
+
+{
+
+printf("%d",a);
+
+}
+
+else if(b>a&&b>c)
+
+{
+
+printf("%d",b);
+
+}
+
+else
+
+{
+
+printf("%d",c);
+
+}
+
+}
+
