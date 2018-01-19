58e4e1702dd7cee1f4130133d417c0458c24cf8f
snivetha81 <31503968+snivetha81@users.noreply.github.com>
2018-01-19 16:06:05 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+        char ch;
+        scanf("%s",&ch);
+        if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+      {
+               printf("alphabet");
+     }
+       else
+    {
+               printf("not an alphabet");
+      }
+       }
