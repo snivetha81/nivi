85e56f4236d6214564e57bd55696ae77e1d15d8d
snivetha81 <31503968+snivetha81@users.noreply.github.com>
2018-02-02 10:40:24 +0000

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,25 @@
+    #include<stdio.h>
+    main()
+    {
+    int i,j,n,t,a[10];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    	scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+    	for(j=i+1;j<n;j++)
+    	{
+    		if(a[i]>a[j])
+    		{
+    			t=a[i];
+    			a[i]=a[j];
+    			a[j]=t;
+    		}
+    	}
+    }
+    	printf("%d",a[n/2]);
+    
+    
+    }
