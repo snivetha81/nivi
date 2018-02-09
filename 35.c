ab92683f03188ac1ae8a475ee60841c7ac7531bc
snivetha81 <31503968+snivetha81@users.noreply.github.com>
2018-02-09 10:32:39 +0000

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+void main()
+{
+    char a[100];
+    int count = 0, i;
+ 
+    printf("enter the string\n");
+    scanf("%[^\n]s",&a);
+    for (i = 0;a[i] != '\0';i++)
+    {
+        if (a[i]>='0'&&a[i]<='9')
+        {
+        count++;
+        }
+   
+}
+ printf("%d", count);
+}
