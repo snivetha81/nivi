e63e24ab1fa5133d998da5f7d59a49a1f29793fc
snivetha81 <31503968+snivetha81@users.noreply.github.com>
2018-03-12 16:14:49 +0530

    Update 63.c

--- 63.c
+++ 63.c
@@ -3,14 +3,14 @@
 void main()
 {
     char a[100];
-    int count = 0, i;
+    int c = 0, i;
  
-    printf("enter the string\n");
+    printf("enter the string");
     scanf("%[^\n]s",&a);
     for (i = 0;a[i] != '\0';i++)
     {
         if (a[i] == ' ')
-            count++;    
+            c++;    
     }
-    printf("%d\n", count + 1);
+    printf("%d\n", c + 1);
 }
